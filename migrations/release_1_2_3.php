e8578d8b1bbf29d6ed97c1c38dd3e3953c459c46
Robert Heim <robert@robert-heim.de>
2014-10-01 23:43:00 +0200

    prepare next dev cycle

---
+++ migrations/release_1_2_3.php
@@ -0,0 +1,41 @@
+<?php
+/**
+*
+* @package phpBB Extension - Activity Stats
+* @copyright (c) 2014 Robet Heim
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace robertheim\activitystats\migrations;
+
+/**
+* @ignore
+*/
+use robertheim\activitystats\PERMISSIONS;
+use robertheim\activitystats\PREFIXES;
+
+class release_1_2_3 extends \phpbb\db\migration\migration
+{
+	protected $version = "1.2.3-DEV";
+
+	public function effectively_installed()
+	{
+		return version_compare($this->config[PREFIXES::CONFIG.'_version'], $this->version, '>=');
+	}
+
+	static public function depends_on()
+	{
+		return array(
+			'\robertheim\activitystats\migrations\release_1_2_2',
+		);
+	}
+
+	public function update_data()
+	{
+		return array(
+			// update version
+			array('config.update', array(PREFIXES::CONFIG.'_version', $this->version)),
+		);
+	}
+}
