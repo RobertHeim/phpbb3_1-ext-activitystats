faba4994a4998914a604415bf93ee9ce973ac1d6
Robert Heim <robert@robert-heim.de>
2014-12-24 16:27:41 +0100

    check that roles exist before adding permissions to them (closes #18)

--- migrations/release_1_2_0.php
+++ migrations/release_1_2_0.php
@@ -33,22 +33,51 @@ class release_1_2_0 extends \phpbb\db\migration\migration
 
 	public function update_data()
 	{
+		// result/return value
+		$re = array();
+		
 		// shortcut
 		$p = PERMISSIONS::SEE_STATS;
-		return array(
-			// add permissions
-			array('permission.add', array($p)),
 
-			// Set permissions for the board roles
-			array('permission.permission_set', array('ROLE_ADMIN_FULL', $p)),
-			array('permission.permission_set', array('ROLE_USER_FULL', $p)),
-			array('permission.permission_set', array('ROLE_USER_STANDARD', $p)),
-
-			// switch indicating if permissions should be checked
-			array('config.add', array(PREFIXES::CONFIG.'_check_permissions', 0)),
-
-			// update version
-			array('config.update', array(PREFIXES::CONFIG.'_version', $this->version)),
-		);
+		// add permissions
+		$re[] = array('permission.add', array($p));
+		
+		// Set permissions for the board roles
+		if ($this->role_exists('ROLE_ADMIN_FULL')) {
+			$re[] = array('permission.permission_set', array('ROLE_ADMIN_FULL', $p));
+		}
+		if ($this->role_exists('ROLE_USER_FULL')) {
+			$re[] = array('permission.permission_set', array('ROLE_USER_FULL', $p));
+		}
+		if ($this->role_exists('ROLE_USER_STANDARD')) {
+			$re[] = array('permission.permission_set', array('ROLE_USER_STANDARD', $p));
+		}
+		
+		// switch indicating if permissions should be checked
+		$re[] = array('config.add', array(PREFIXES::CONFIG.'_check_permissions', 0));
+		
+		// update version
+		$re[] = array('config.update', array(PREFIXES::CONFIG.'_version', $this->version));
+		
+		return $re;
 	}
+	
+	/**
+	 * Checks whether the given role does exist or not.
+	 *
+	 * @param String $role the name of the role
+	 * @return true if the role exists, false otherwise.
+	 */
+	private function role_exists($role) {
+		$sql_array = array(
+			'SELECT'	=> '1 AS found',
+			'FROM'		=> array(ACL_ROLES_TABLE => 'r'),
+			'WHERE'		=> $this->db->sql_in_set('r.role_name', array($role)),
+		);
+		$sql = $this->db->sql_build_query('SELECT', $sql_array);
+		$result = $this->db->sql_query($sql);
+		$found = (int) $this->db->sql_fetchfield('found');
+		return ($found > 0);
+	}
+	
 }
