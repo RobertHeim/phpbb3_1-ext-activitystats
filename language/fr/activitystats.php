b337bb3e7c50dc4949d909240dd19a4e4a034fe2
ForumsFaciles <forumsfaciles68000@gmail.com>
2015-02-04 02:01:42 +0100

    French translation
    First version

---
+++ language/fr/activitystats.php
@@ -0,0 +1,87 @@
+<?php
+/**
+*
+* @package phpBB Extension - Activity Stats
+* @copyright (c) 2014 Robet Heim
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+* French translation by ForumsFaciles (http://www.forumsfaciles.fr) - v.1.0.0 - 2015-02-04 --- 1:43am
+*/
+
+if (!defined('IN_PHPBB'))
+{
+    exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+    $lang = array();
+}
+
+$lang = array_merge($lang, array(
+// for the normal sites
+	'ACTIVITY_STATS'					=> 'Statistiques d’activité',
+	'ACTIVITY_STATS_LATEST1'			=> 'dernier le',
+	'ACTIVITY_STATS_LATEST2'			=> '',//used for parts like o'clock in the timedisplay (last at vw:xy "o'clock")
+
+	'ACTIVITY_STATS_NEW_TOPICS'			=> 'Nouveau(x) sujet(s) : <strong>%d</strong>',
+	'ACTIVITY_STATS_NEW_POSTS'			=> 'Nouveau(x) message(s) : <strong>%d</strong>',
+	'ACTIVITY_STATS_NEW_USERS'			=> 'Nouveau(x) membre(s) : <strong>%d</strong>',
+
+	'ACTIVITY_STATS_TOTAL'				=> array(
+		0		=> 'Il n’y avait <strong>aucun</strong> utilisateur en ligne : ',
+		1		=> 'Il y avait <strong>%d</strong> utilisateur en ligne : ',
+		2		=> 'Il y avait <strong>%d</strong> utilisateurs en ligne : ',
+	),
+	'ACTIVITY_STATS_REG_USERS'			=> array(
+		0		=> 'aucun membre',
+		1		=> '%d membre',
+		2		=> '%d membres',
+	),
+	'ACTIVITY_STATS_HIDDEN'				=> array(
+		0		=> 'aucun masqué',
+		1		=> '%d masqué',
+		2		=> '%d masqués',
+	),
+	'ACTIVITY_STATS_BOTS'				=> array(
+		0		=> 'aucun robot',
+		1		=> '%d robot',
+		2		=> '%d robots',
+	),
+	'ACTIVITY_STATS_GUESTS'				=> array(
+		0		=> 'aucun invité',
+		1		=> '%d invité',
+		2		=> '%d invités',
+	),
+
+	'ACTIVITY_STATS_WORD'				=> ' et',
+	'ACTIVITY_STATS_EXP'				=> 'Les données suivantes sont basées sur les utilisateurs actifs aujourd’hui',
+	'ACTIVITY_STATS_EXP_TIME'			=> array(
+		0		=> 'Les données suivantes sont basées sur les utilisateurs actifs à l’instant', // d=h=m=s=0 
+		1		=> 'Les données suivantes sont basées sur les utilisateurs actifs durant la dernière ', // first non zero value is 1
+		2		=> 'Les données suivantes sont basées sur les utilisateurs actifs durant les dernières ', // first non zero value is >1
+	),
+	'ACTIVITY_STATS_DAYS'				=> array(
+		0		=> '',
+		1		=> '%%s %1$s jour',
+		2		=> '%%s %1$s jours',
+	),
+	'ACTIVITY_STATS_HOURS'				=> array(
+		0		=> '',
+		1		=> '%%s %1$s heure',
+		2		=> '%%s %1$s heures',
+	),
+	'ACTIVITY_STATS_MINUTES'			=> array(
+		0		=> '',
+		1		=> '%%s %1$s minute',
+		2		=> '%%s %1$s minutes',
+	),
+	'ACTIVITY_STATS_SECONDS'			=> array(
+		0		=> '',
+		1		=> '%%s %1$s seconde',
+		2		=> '%%s %1$s secondes',
+	),
+	'ACTIVITY_STATS_RECORD_DAY'			=> 'Le nombre maximum d’utilisateurs en ligne était de <strong>%1$s</strong> le %2$s',
+	'ACTIVITY_STATS_RECORD_PERIOD'		=> 'Le nombre maximum d’utilisateurs en ligne était de <strong>%1$s</strong> entre le %2$s et le %3$s',
+));
+
---
+++ language/fr/permissions_activitystats.php
@@ -0,0 +1,45 @@
+<?php
+/**
+*
+* @package phpBB Extension - Activity Stats
+* @copyright (c) 2014 Robet Heim
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+* French translation by ForumsFaciles (http://www.forumsfaciles.fr) - v.1.0.0 - 2015-02-04 --- 1:43am
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ » “ ” …
+//
+
+use robertheim\activitystats\PERMISSIONS;
+
+$lang = array_merge($lang, array(
+	'ACL_'.mb_strtoupper(PERMISSIONS::SEE_STATS)	=> 'Peut consulter les statistiques d’activité',
+));
+
