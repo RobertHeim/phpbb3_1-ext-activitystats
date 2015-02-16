01c3cee27fa88b35d75943fb643e75f90c4e2746
Robert Heim <robert@robert-heim.de>
2015-02-16 22:53:19 +0100

    proper refactorings

--- event/main_listener.php
+++ event/main_listener.php
@@ -13,9 +13,9 @@ namespace robertheim\activitystats\event;
 * @ignore
 */
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use robertheim\activitystats\MODES;
-use robertheim\activitystats\PERMISSIONS;
-use robertheim\activitystats\PREFIXES;
+use robertheim\activitystats\modes;
+use robertheim\activitystats\permissions;
+use robertheim\activitystats\prefixes;
 
 /**
 * Event listener
@@ -53,13 +53,13 @@ class main_listener implements EventSubscriberInterface
 	{
 		global $config, $auth;
 		$this->sessions_manager->update_session();
-		if (!$config[PREFIXES::CONFIG.'_check_permissions'] || $auth->acl_get(PERMISSIONS::SEE_STATS))
+		if (!$config[prefixes::CONFIG.'_check_permissions'] || $auth->acl_get(permissions::SEE_STATS))
 		{
 			$this->user->add_lang_ext('robertheim/activitystats', 'activitystats');
 
 			// find timeperiod: today (mode=1) or other configured time period (mode=2)
 			$timestamp = 0;
-			if (MODES::TODAY == $config[PREFIXES::CONFIG . '_mode'])
+			if (modes::TODAY == $config[prefixes::CONFIG . '_mode'])
 			{
 				// today
 				// we calculate timestamp of midnight in the users timezone
@@ -78,10 +78,10 @@ class main_listener implements EventSubscriberInterface
 			{
 				// use UTC for period
 				$timestamp = time();
-				$timestamp -= (86400 * $config[PREFIXES::CONFIG . '_del_time_d']);
-				$timestamp -= ( 3600 * $config[PREFIXES::CONFIG . '_del_time_h']);
-				$timestamp -= (   60 * $config[PREFIXES::CONFIG . '_del_time_m']);
-				$timestamp -=          $config[PREFIXES::CONFIG . '_del_time_s'];
+				$timestamp -= (86400 * $config[prefixes::CONFIG . '_del_time_d']);
+				$timestamp -= ( 3600 * $config[prefixes::CONFIG . '_del_time_h']);
+				$timestamp -= (   60 * $config[prefixes::CONFIG . '_del_time_m']);
+				$timestamp -=          $config[prefixes::CONFIG . '_del_time_s'];
 				// prune everything before the period
 				$this->sessions_manager->prune($timestamp);
 			}
@@ -90,7 +90,7 @@ class main_listener implements EventSubscriberInterface
 			$this->sessions_manager->check_record();
 
 			// don't re-calculate the data within that time, but use the cached data from the last calculation.
-			$cachetime = $config[PREFIXES::CONFIG . '_cache_time'];
+			$cachetime = $config[prefixes::CONFIG . '_cache_time'];
 
 			// calculate the data to display or read it from the cache
 			$activity = $this->sessions_manager->obtain_data($timestamp, $cachetime);
@@ -105,21 +105,21 @@ class main_listener implements EventSubscriberInterface
 	{
 		global $config, $auth, $user;
 
-		if ($config[PREFIXES::CONFIG . '_disp_new_topics'])
+		if ($config[prefixes::CONFIG . '_disp_new_topics'])
 		{
 			$this->template->assign_vars(array(
 				'ACTIVITY_STATS_NEW_TOPICS'			=> $this->user->lang('ACTIVITY_STATS_NEW_TOPICS', $activity['new_topics']),
 			));
 		}
 
-		if ($config[PREFIXES::CONFIG . '_disp_new_posts'])
+		if ($config[prefixes::CONFIG . '_disp_new_posts'])
 		{
 			$this->template->assign_vars(array(
 				'ACTIVITY_STATS_NEW_POSTS'			=> $this->user->lang('ACTIVITY_STATS_NEW_POSTS', $activity['new_posts']),
 			));
 		}
 
-		if ($config[PREFIXES::CONFIG . '_disp_new_users'])
+		if ($config[prefixes::CONFIG . '_disp_new_users'])
 		{
 			$this->template->assign_vars(array(
 				'ACTIVITY_STATS_NEW_USERS'			=> $this->user->lang('ACTIVITY_STATS_NEW_USERS', $activity['new_users']),
@@ -129,21 +129,21 @@ class main_listener implements EventSubscriberInterface
 		$users_list = '';
 		foreach ($activity['users_list'] as $key => $row)
 		{
-			$hover_time = (($config[PREFIXES::CONFIG . '_disp_time'] == '2') ? $user->lang['ACTIVITY_STATS_LATEST1'] . '&nbsp;' . $user->format_date($row['lastpage'], $config[PREFIXES::CONFIG . '_disp_time_format']) . $user->lang['ACTIVITY_STATS_LATEST2'] : '' );
-			$hover_ip = ($auth->acl_get('a_') && $config[PREFIXES::CONFIG . '_disp_ip']) ? $user->lang['IP'] . ':&nbsp;' . $row['user_ip'] : '';
+			$hover_time = (($config[prefixes::CONFIG . '_disp_time'] == '2') ? $user->lang['ACTIVITY_STATS_LATEST1'] . '&nbsp;' . $user->format_date($row['lastpage'], $config[prefixes::CONFIG . '_disp_time_format']) . $user->lang['ACTIVITY_STATS_LATEST2'] : '' );
+			$hover_ip = ($auth->acl_get('a_') && $config[prefixes::CONFIG . '_disp_ip']) ? $user->lang['IP'] . ':&nbsp;' . $row['user_ip'] : '';
 			$hover_info = (($hover_time || $hover_ip) ? ' title="' . $hover_time . (($hover_time && $hover_ip) ? ' | ' : '') . $hover_ip . '"' : '');
-			$disp_time = (($config[PREFIXES::CONFIG . '_disp_time'] == '1') ? '&nbsp;(' . $user->lang['ACTIVITY_STATS_LATEST1'] . '&nbsp;' . $user->format_date($row['lastpage'], $config[PREFIXES::CONFIG . '_disp_time_format']) . $user->lang['ACTIVITY_STATS_LATEST2'] . (($hover_ip) ? ' | ' . $hover_ip : '' ) . ')' : '' );
+			$disp_time = (($config[prefixes::CONFIG . '_disp_time'] == '1') ? '&nbsp;(' . $user->lang['ACTIVITY_STATS_LATEST1'] . '&nbsp;' . $user->format_date($row['lastpage'], $config[prefixes::CONFIG . '_disp_time_format']) . $user->lang['ACTIVITY_STATS_LATEST2'] . (($hover_ip) ? ' | ' . $hover_ip : '' ) . ')' : '' );
 
 			// if not hidden user
 			if ($row['viewonline'] || ($row['user_type'] == USER_IGNORE))
 			{
-				if (($row['user_id'] != ANONYMOUS) && ($config[PREFIXES::CONFIG . '_disp_bots'] || ($row['user_type'] != USER_IGNORE)))
+				if (($row['user_id'] != ANONYMOUS) && ($config[prefixes::CONFIG . '_disp_bots'] || ($row['user_type'] != USER_IGNORE)))
 				{
 					$users_list .= $user->lang['COMMA_SEPARATOR'] . '<span' . $hover_info . '>' . $row['username'] . '</span>' . $disp_time;
 					$ids_user[] = $row['user_id'];
 				}
 			}
-			else if (($config[PREFIXES::CONFIG . '_disp_hidden']) && ($auth->acl_get('u_viewonline')))
+			else if (($config[prefixes::CONFIG . '_disp_hidden']) && ($auth->acl_get('u_viewonline')))
 			{
 				$users_list .= $user->lang['COMMA_SEPARATOR'] . '<em' . $hover_info . '>' .$row['username'] . '</em>' . $disp_time;
 				$ids_user[] = $row['user_id'];
@@ -158,7 +158,7 @@ class main_listener implements EventSubscriberInterface
 		}
 
 		$record_string = '';
-		if ($config[PREFIXES::CONFIG . '_record'])
+		if ($config[prefixes::CONFIG . '_record'])
 		{
 			$record_string = $this->get_record_string();
 		}
@@ -167,7 +167,7 @@ class main_listener implements EventSubscriberInterface
 			'ACTIVITY_STATS_TOTAL_NEWS' => $this->get_total_news_string($activity),
 			'ACTIVITY_STATS_LIST'		=> $user->lang['REGISTERED_USERS'] . ' ' . $users_list,
 			'ACTIVITY_STATS_TOTAL'		=> $this->get_total_users_string($activity),
-			'ACTIVITY_STATS_EXP'		=> $this->get_explanation_string($config[PREFIXES::CONFIG . '_mode']),
+			'ACTIVITY_STATS_EXP'		=> $this->get_explanation_string($config[prefixes::CONFIG . '_mode']),
 			'ACTIVITY_STATS_RECORD'		=> $record_string,
 		));
 	}
@@ -187,10 +187,10 @@ class main_listener implements EventSubscriberInterface
 		}
 		else
 		{
-			$d = (int) $config[PREFIXES::CONFIG . '_del_time_d'];
-			$h = (int) $config[PREFIXES::CONFIG . '_del_time_h'];
-			$m = (int) $config[PREFIXES::CONFIG . '_del_time_m'];
-			$s = (int) $config[PREFIXES::CONFIG . '_del_time_s'];
+			$d = (int) $config[prefixes::CONFIG . '_del_time_d'];
+			$h = (int) $config[prefixes::CONFIG . '_del_time_h'];
+			$m = (int) $config[prefixes::CONFIG . '_del_time_m'];
+			$s = (int) $config[prefixes::CONFIG . '_del_time_s'];
 
 			$plural = $d;
 			if (0==$d)
@@ -232,25 +232,25 @@ class main_listener implements EventSubscriberInterface
 	{
 		global $config, $user;
 
-		$format = $config[PREFIXES::CONFIG . '_record_timeformat'];
+		$format = $config[prefixes::CONFIG . '_record_timeformat'];
 
-		if (MODES::TODAY == $config[PREFIXES::CONFIG . '_mode'])
+		if (modes::TODAY == $config[prefixes::CONFIG . '_mode'])
 		{
 			return sprintf($user->lang['ACTIVITY_STATS_RECORD_DAY'],
-				$config[PREFIXES::CONFIG . '_record_count'],
-				$user->format_date($config[PREFIXES::CONFIG . '_record_time'], $format)) . '<br />';
+				$config[prefixes::CONFIG . '_record_count'],
+				$user->format_date($config[prefixes::CONFIG . '_record_time'], $format)) . '<br />';
 		}
 		else
 		{
-			$period_start = $config[PREFIXES::CONFIG . '_record_time'];
-			$period_start -= (86400 * $config[PREFIXES::CONFIG . '_del_time_d']);
-			$period_start -= ( 3600 * $config[PREFIXES::CONFIG . '_del_time_h']);
-			$period_start -= (   60 * $config[PREFIXES::CONFIG . '_del_time_m']);
-			$period_start -=          $config[PREFIXES::CONFIG . '_del_time_s'];
+			$period_start = $config[prefixes::CONFIG . '_record_time'];
+			$period_start -= (86400 * $config[prefixes::CONFIG . '_del_time_d']);
+			$period_start -= ( 3600 * $config[prefixes::CONFIG . '_del_time_h']);
+			$period_start -= (   60 * $config[prefixes::CONFIG . '_del_time_m']);
+			$period_start -=          $config[prefixes::CONFIG . '_del_time_s'];
 			return sprintf($user->lang['ACTIVITY_STATS_RECORD_PERIOD'],
-				$config[PREFIXES::CONFIG . '_record_count'],
+				$config[prefixes::CONFIG . '_record_count'],
 				$user->format_date($period_start, $format),
-				$user->format_date($config[PREFIXES::CONFIG . '_record_time'], $format)) . '<br />';
+				$user->format_date($config[prefixes::CONFIG . '_record_time'], $format)) . '<br />';
 		}
 	}
 
@@ -263,15 +263,15 @@ class main_listener implements EventSubscriberInterface
 		global $config, $user;
 
 		$total_news_string = array();
-		if ($config[PREFIXES::CONFIG . '_disp_new_topics'])
+		if ($config[prefixes::CONFIG . '_disp_new_topics'])
 		{
 			$total_news_string[] = $user->lang('ACTIVITY_STATS_NEW_TOPICS', $activity['new_topics']);
 		}
-		if ($config[PREFIXES::CONFIG . '_disp_new_posts'])
+		if ($config[prefixes::CONFIG . '_disp_new_posts'])
 		{
 			$total_news_string[] = $user->lang('ACTIVITY_STATS_NEW_POSTS', $activity['new_posts']);
 		}
-		if ($config[PREFIXES::CONFIG . '_disp_new_users'])
+		if ($config[prefixes::CONFIG . '_disp_new_users'])
 		{
 			$total_news_string[] = $user->lang('ACTIVITY_STATS_NEW_USERS', $activity['new_users']);
 		}
@@ -288,15 +288,15 @@ class main_listener implements EventSubscriberInterface
 
 		$total_users_string = $user->lang('ACTIVITY_STATS_TOTAL', $activity['count_total']);
 		$total_users_string .= $user->lang('ACTIVITY_STATS_REG_USERS', $activity['count_reg']);
-		if ($config[PREFIXES::CONFIG . '_disp_hidden'])
+		if ($config[prefixes::CONFIG . '_disp_hidden'])
 		{
 			$total_users_string .= '%s ' . $user->lang('ACTIVITY_STATS_HIDDEN', $activity['count_hidden']);
 		}
-		if ($config[PREFIXES::CONFIG . '_disp_bots'])
+		if ($config[prefixes::CONFIG . '_disp_bots'])
 		{
 			$total_users_string .= '%s ' . $user->lang('ACTIVITY_STATS_BOTS', $activity['count_bot']);
 		}
-		if ($config[PREFIXES::CONFIG . '_disp_guests'])
+		if ($config[prefixes::CONFIG . '_disp_guests'])
 		{
 			$total_users_string .= '%s ' . $user->lang('ACTIVITY_STATS_GUESTS', $activity['count_guests']);
 		}
